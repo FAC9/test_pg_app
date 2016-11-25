234b5105e7e7d2dff42ee17e499923b873c99432
Tom Barrett <tbtommyb@gmail.com>
2016-11-25 12:15:22 +0000

    First commit

---
+++ build_database.sql
@@ -0,0 +1,23 @@
+BEGIN;
+
+DROP TABLE IF EXISTS people cascade;
+
+CREATE TABLE people (
+  id           SERIAL        PRIMARY KEY NOT NULL,
+  firstName    VARCHAR(100)  NOT NULL,
+  surname      VARCHAR(100)  NOT NULL,
+  superpower   VARCHAR(100)  NOT NULL,
+  age          SMALLINT      NOT NULL,
+  glasses      BOOLEAN       NOT NULL    DEFAULT  TRUE
+);
+
+INSERT INTO people(firstName, surname, superpower, age, glasses) VALUES (
+  'Marina', 'Sideri', 'Mind-reading', 29);
+INSERT INTO people(firstName, surname, superpower, age, glasses) VALUES (
+  'Tom', 'Barrett', 'Flight', 27);
+INSERT INTO people(firstName, surname, superpower, age, glasses) VALUES (
+  'Will', 'Savage', 'Invisibility', 24, false);
+INSERT INTO people(firstName, surname, superpower, age, glasses) VALUES (
+  'Nori', 'Denes', 'Seeing the future', 24);
+
+COMMIT;
